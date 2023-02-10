52a499844ad1887b07c720dc4b5f5ad575566052
arigunawanj <arigunawanjatmiko@gmail.com>
2023-02-10 17:00:50 +0700

    portofolio

--- script.js
+++ script.js
@@ -62,3 +62,17 @@ $(selector).on('click', function(){
     $(selector).removeClass('active');
     $(this).addClass('active');
 });
+
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
